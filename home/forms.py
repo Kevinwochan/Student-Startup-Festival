24adfa2d511f9d7cb5522150855e06c03468ca0f
Ubuntu <ubuntu@ip-172-31-3-227.ap-southeast-2.compute.internal>
2018-08-22 08:20:56 +0000

    removed html temlplate for form

--- home/forms.py
+++ home/forms.py
@@ -11,3 +11,19 @@ class applicationForm(forms.Form):
     university = forms.ChoiceField(choices=UNIVERSITY_CHOICES);
     pdf = forms.FileField()
     comments = forms.CharField(widget=forms.Textarea)
+
+
+
+#	<form id="startupcup-application" enctype="multipart/form-data"action="{% url 'application' %}" method="post">
+#		<div class="form-group">
+#				<label for="InputEmail">Email address</label>
+#				<input type="email" class="form-control" id="InputEmail" aria-describedby="emailHelp" placeholder="Enter email">
+#			</div>
+#			<div class="form-group">
+#				<label for="InputPassword">Password</label>
+#				<input type="password" class="form-control" id="InputPassword" placeholder="Password">
+#			</div>
+#			<label for="FormControlFile"> file input</label>
+#			<input type="file" class="form-control-file" id="FormControlFile">
+#			<button type="submit" class="btn btn-primary">Submit</button>
+#		</form>
