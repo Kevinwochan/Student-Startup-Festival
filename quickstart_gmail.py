3286723f8bdf1fe845e601f4ab71b532931a37ef
Kevin <kevinwochan@gmail.com>
2018-08-26 00:51:29 +1000

    messing with google credentials to fix 403 permission erorr

--- quickstart_gmail.py
+++ quickstart_gmail.py
@@ -4,7 +4,7 @@ from httplib2 import Http
 from oauth2client import file, client, tools
 
 # If modifying these scopes, delete the file token.json.
-SCOPES = 'https://www.googleapis.com/auth/gmail.compose'
+SCOPES = 'https://mail.google.com/'
 
 def main():
     """Shows basic usage of the Gmail API.
@@ -13,7 +13,7 @@ def main():
     store = file.Storage('token.json')
     creds = store.get()
     if not creds or creds.invalid:
-        flow = client.flow_from_clientsecrets('home/credentials.json', SCOPES)
+        flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
         creds = tools.run_flow(flow, store)
     service = build('gmail', 'v1', http=creds.authorize(Http()))
 
