79dc0ca1bf47cb794fe710619b1b61ad076bef34
kevin chan <kevinwochan@gmail.com>
2018-08-29 22:17:00 +1000

    addign repo name in update script

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
-echo "=== log entry $timestamp ==="
+echo "=== Updating SSF Repo: log entry $timestamp ==="
 cd ~/Student-Startup-Festival;
 git pull;
 echo "==========================="
