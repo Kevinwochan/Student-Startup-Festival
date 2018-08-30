57c3eb486ec1d57ca5c263cedbaf031422ea4595
Ubuntu <ubuntu@ip-172-31-3-227.ap-southeast-2.compute.internal>
2018-08-30 02:47:52 +0000

    fixed settings with media folder

--- StudentStartupFestival/settings.py
+++ StudentStartupFestival/settings.py
@@ -134,10 +134,10 @@ STATICFILES_DIRS = (
                 os.path.join(BASE_DIR, 'static'),
                 )
 
-MEDIA_ROOT = os.path.join(BASE_DIR, '/media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-EMAIL_FILE_PATH = '/emails/'
+EMAIL_FILE_PATH = '/emails'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
