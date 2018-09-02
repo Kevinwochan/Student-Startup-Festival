5dd2835bd1ecad0168f49504b321ff406a64457b
Ubuntu <ubuntu@ip-172-31-3-227.ap-southeast-2.compute.internal>
2018-09-02 18:19:39 +0000

    auto chown

--- update.sh
+++ update.sh
@@ -4,6 +4,7 @@ timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
 echo "=== Updating SSF Repo: log entry $timestamp ==="
 cd ~/Student-Startup-Festival;
 git pull;
+chown -R :www-data .
 pipenv run python manage.py migrate --no-input
 pipenv run python manage.py makemigrations --no-input
 pipenv run python manage.py collectstatic --no-input --clear
